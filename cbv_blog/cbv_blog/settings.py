7f8ee1aefd01367814606b1d44b0c38def1716dd
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-12-30 15:30:28 +0530

    hosting

--- cbv_blog/cbv_blog/settings.py
+++ cbv_blog/cbv_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$htedxvg=czq$&-+!g)=ooja36wg#uq62dwo#bxho+^&xpt6wf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
